d6e6aa3d6cd6d12b785b487a63b71b13aa4c1d1e
Wes Vetter <wes.vetter@gmail.com>
2014-02-13 15:45:30 -0800

    Add an Rspec/Zeus alias

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -4,15 +4,24 @@
 # whatever files or links were there previously, without backing them up.
 # #YOLO.
 
-topics="vim shell ruby git"
 
 echo -n "Creating new symlinks..."
 
+# copy topic folders
+topics="vim shell ruby git"
 for topic in $topics; do
   cd $HOME/.dotfiles/$topic
   for file in `ls`; do
     ln -sf $HOME/.dotfiles/$topic/$file $HOME/.$file
+    echo "ln -sf $HOME/.dotfiles/$topic/$file $HOME/.$file"
   done
+done
+
+# copy "binary" files
+cd $HOME/.dotfiles/bin
+for bin in `ls`; do
+  ln -sf $HOME/.dotfiles/bin/$bin $HOME/.bin/$bin
+  #echo "ln -sf $HOME/.dotfiles/bin/$bin $HOME/.bin/$bin"
 done
 
 echo "done"
