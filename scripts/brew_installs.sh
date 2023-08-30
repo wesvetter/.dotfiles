e456c5d7c06eb50c688021eab723281fb4889677
Wes Vetter <wes.vetter@gmail.com>
2023-08-29 22:34:30 -0700

    Simplify Homebrew installs
    Homebrew is installed on macOS by default now so it doesn't need to be
    installed first.
    
    Also add a few more packages to the default list.

--- scripts/brew_installs.sh
+++ scripts/brew_installs.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# Requires homebrew to be installed first.
-# TODO: figure out how to skip homebrew install and just install packages
-# (for updating)
-
-# install homebrew
-#echo "Installing homebrew..."
-#ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-#echo "Finished installing homebrew."
-
-# TODO: should probably do some brew doctor check before proceeding
-
 # It might be nice to install postgres, mysql, and openssl but leaving
 # those off as they aren't always needed.
-packages="ack hub tmux tree wget fzf"
+packages="ack hub tmux tree wget fzf jq readline reattach-to-user-namespace zsh"
 
 for package in $packages; do
   brew install $package
