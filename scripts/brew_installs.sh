9389effc9905bb5401b582fe04b4e70f980995f8
Wes Vetter <wes.vetter@gmail.com>
2025-03-27 15:23:20 -0700

    ✨ Reformat SQL in Vim with pg_format
    Adds a leader command (leader+pf) to prettify a selection in visual mode
    using the `pg_format` utility.
    
    To get the `pg_format` utility run:
    
    ```
    brew install pgformatter
    ```

--- scripts/brew_installs.sh
+++ scripts/brew_installs.sh
@@ -2,7 +2,7 @@
 
 # It might be nice to install postgres, mysql, and openssl but leaving
 # those off as they aren't always needed.
-packages="git git-gui ack hub tmux tree wget fzf jq readline reattach-to-user-namespace"
+packages="git git-gui ack hub tmux tree wget fzf jq readline reattach-to-user-namespace pgformatter"
 
 for package in $packages; do
   brew install $package
